ad2470c93d52cbaf2e3d84de431707949f2fda74
hanjeong Lee <gkswjdzz@comcom.ai>
2020-10-27 22:16:46 +0900

    add server

--- server.py
+++ server.py
@@ -5,19 +5,33 @@ import json
 
 app = Flask(__name__)
 
-SERVER='https://length-1-gpt-2-large-tf-serving-gkswjdzz.endpoint.ainize.ai/v1/models/gpt-2-large:predict'
-
+SERVERS = {
+  'length-1': 'https://length-1-gpt-2-large-tf-serving-gkswjdzz.endpoint.ainize.ai/v1/models/gpt-2-large:predict',
+  'length-x': 'https://main-gpt-2-large-tf-serving-gkswjdzz.endpoint.ainize.ai/v1/models/gpt-2-large:predict'
+}
 @app.route('/large', methods=['POST'])
 def large():
   keys = list(request.form.keys())
 
-  if len(keys) != 1 :
+  if len(keys) != 2 :
     return jsonify({'message': 'invalid body'}), 400
   
   raw_text_key = list(request.form.keys())[0]
+  length_key = list(request.form.keys())[1] 
+
   print(raw_text_key)
   raw_text = request.form[raw_text_key]
+  
+  length = request.form[length_key]
+  
+  print(('length-'+length))
+  if not(('length-'+ length) in SERVERS.keys()):
+    return jsonify({'message': 'body error'}), 400
+  
+  
   print(encode(raw_text))
+  print(length, type(length))
+
   
   data = {
     'signature_name':'predict',
@@ -25,10 +39,11 @@ def large():
       encode(raw_text)
     ]
   }
-  res = requests.post(SERVER, data=json.dumps(data))
-  
-  if res.status_code is not 200:
-    return jsonify({'error'}), res.status_code
+
+  res = requests.post(SERVERS['length-'+length], data=json.dumps(data))
+
+  if res.status_code != 200:
+    return jsonify({'message': 'error'}), res.status_code
   r = res.json()
   return decode(r['predictions']), 200
 
