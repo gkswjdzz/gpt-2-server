c8175edad76f33d7e02b1651d07858ef5d0d20b7
Hanjeong Lee <gkswjdzz@naver.com>
2021-03-10 15:03:31 +0900

    register model when server is started

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,14 @@
 
 wget -O model_download_link.txt $MODEL_DOWNLOAD_LINK
 
-head -n 5 model_download_link.txt > model_download_link2.txt
-/bin/bash model_download.sh model_download_link2.txt
+/bin/bash model_download.sh model_download_link.txt
 
-torchserve --start --ncs --model-store=/home/model-server/model-store \
-& python server.py
+torchserve --start --ncs --model-store=${PWD}/model-store &
+sleep 15
+
+apt install -y curl
+ls -1 model-store | while read line; do
+  curl -X POST "http://localhost:8081/models?url=${line}" 
+  sleep 20
+done
+python server.py
