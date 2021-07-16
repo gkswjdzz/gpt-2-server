57f30079a046ad2afee3a9684547f9e4bc868dae
Hanjeong Lee <gkswjdzz@naver.com>
2021-07-16 16:43:11 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
-curl -X GET $MODEL_DOWNLOAD_LINK -o model.list
-while read line; do
-  echo $line | xargs basename | sed 's/%2F/\//g' | awk -F'[///?]' -v var=$line '{ print var, $3 }' | awk -v q="'" '{print "curl "q $1 q" -o model-store/" $2}' | bash
-done < model.list
-cat model.list
-ls -1 model-store | while read line; do
-  curl -X POST "http://localhost:8081/models?initial_workers=1&synchronous=true&url=${line}"
-  sleep 20
-done
+# curl -X GET $MODEL_DOWNLOAD_LINK -o model.list
+# while read line; do
+#   echo $line | xargs basename | sed 's/%2F/\//g' | awk -F'[///?]' -v var=$line '{ print var, $3 }' | awk -v q="'" '{print "curl "q $1 q" -o model-store/" $2}' | bash
+# done < model.list
+# cat model.list
+# ls -1 model-store | while read line; do
+#   curl -X POST "http://localhost:8081/models?initial_workers=1&synchronous=true&url=${line}"
+#   sleep 20
+# done
 torchserve --start --ncs --model-store=/home/model-server/model-store --models=all
